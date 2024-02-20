4272235ac8bf0d6137b77ff9a443b06c163c8fcc
{jinsung} <jayhan0215@gmail.com>
2024-02-20 19:58:29 +0900

    Reset the Auth page for styling

--- lib/replacement.dart
+++ lib/replacement.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:workout_app/styles/font_inter.dart';
 
 class Auth extends StatefulWidget {
   const Auth({super.key});
@@ -14,56 +12,24 @@ class _AuthState extends State<Auth> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  width: 2,
-                  color: Colors.red,
-                ),
-              ),
-              child: SizedBox(
-                child: Stack(
-                  children: [
-                    Row(
-                      children: [
-                        const Column(
-                          children: [
-                            FontInter(
-                              content: "workout",
-                              fontSize: 80,
-                              isBold: true,
-                            ),
-                            FontInter(
-                              content: "Make your working out",
-                              fontSize: 20,
-                              color: Color(0xFF838383),
-                            ),
-                          ],
-                        ),
-                        Container(
-                          decoration: const BoxDecoration(
-                            color: Colors.blue,
-                          ),
-                          child: const SizedBox(
-                            width: 70,
-                            height: 60,
-                          ),
-                        )
-                      ],
-                    ),
-                    Positioned(
-                      child: SvgPicture.asset(
-                        'assets/svg/dumbell.svg',
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          ],
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.blue,
+              width: 5,
+            ),
+          ),
+          child: const SizedBox.expand(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Text("data1"),
+                Text("data2"),
+                Text("data3"),
+                Text("data4"),
+              ],
+            ),
+          ),
         ),
       ),
     );
