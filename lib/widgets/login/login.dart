6c6453d63a8da7c2ecddbc34f9e853b56e48fd4c
{jinsung} <jayhan0215@gmail.com>
2024-03-19 00:34:36 +0900

    Login (2)

--- lib/widgets/login/login.dart
+++ lib/widgets/login/login.dart
@@ -1,54 +1,13 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+Future<void> loginUser(String userId, String password) async {
+  Dio dio = Dio();
 
-  @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  TextEditingController id = TextEditingController();
-  TextEditingController pw = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    login() async {
-      if (id.text != "" && pw.text != "") {
-        try {
-          Dio dio = Dio();
-
-          Response res = await dio.post(
-            "path",
-            data: {
-              "userId": id.text,
-              "password": pw.text,
-            },
-          );
-        } catch (e) {
-          showSnackBar(context, "Login failed 🥲");
-        }
-      }
-    }
-
-    return const Placeholder();
-  }
-}
-
-void showSnackBar(BuildContext context, String content) {
-  ScaffoldMessenger.of(context).showSnackBar(
-    SnackBar(
-      content: Text(
-        content,
-        textAlign: TextAlign.center,
-        style: const TextStyle(
-          fontSize: 16,
-        ),
-      ),
-      duration: const Duration(
-        seconds: 2,
-      ),
-    ),
+  Response res = await dio.post(
+    "path",
+    data: {
+      "userId": userId,
+      "password": password,
+    },
   );
 }
