8d4fbd5cbbdd999ea2c8701336586ee35d02634c
{jinsung} <jayhan0215@gmail.com>
2024-04-10 19:27:24 +0900

    add screen app bar

--- lib/widgets/home/add_work_screen.dart
+++ lib/widgets/home/add_work_screen.dart
@@ -8,6 +8,24 @@ class AddWorkScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
+        leadingWidth: MediaQuery.of(context).size.width,
+        leading: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.arrow_back_ios_new_rounded,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add,
+              ),
+            ),
+          ],
+        ),
       ),
       body: const Center(
         child: Text("ADD WORK SCREEN"),
