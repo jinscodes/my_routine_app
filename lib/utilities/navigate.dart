9858ac58858bd87bfacf1fdb013884ed01e14184
{jinsung} <jayhan0215@gmail.com>
2024-06-10 12:44:43 +0900

    CompleteType

--- lib/utilities/navigate.dart
+++ lib/utilities/navigate.dart
@@ -14,6 +14,10 @@ enum NavigateType {
   pushRep,
 }
 
+enum CompleteType {
+  complete,
+}
+
 class Navigate {
   BuildContext context;
   WidgetBuilder? builder;
@@ -65,6 +69,7 @@ class Navigate {
 class NavigateTo {
   BuildContext context;
   NavigateType? type;
+
   Map<dynamic, dynamic>? params;
 
   NavigateTo({
