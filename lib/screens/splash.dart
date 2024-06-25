2033e42730446ecbdd901ff657e3e78918c5b47d
{jinsung} <jayhan0215@gmail.com>
2024-06-25 13:02:18 +0900

    Make manageLoginToken logic and throughTimer

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,7 +1,30 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+import '../trash/utilities/manageLoginToken.dart';
+
+class Splash extends StatefulWidget {
   const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  void _checkLoginToken() async {
+    String? loginToken = await getLoginToken();
+
+    if (loginToken == null || loginToken.isEmpty) {
+    } else {}
+  }
+
+  void throughTimer(Function navigate) {
+    Timer(
+      const Duration(seconds: 2),
+      () => navigate(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
