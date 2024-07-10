dce7acf16c1491a22e420988f359b4f79d834663
{jinsung} <jayhan0215@gmail.com>
2024-07-11 01:47:08 +0900

    login suffix logic

---
+++ lib/widgets/login_outline/login_input.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginInput extends StatefulWidget {
+  const LoginInput({super.key});
+
+  @override
+  State<LoginInput> createState() => _LoginInputState();
+}
+
+class _LoginInputState extends State<LoginInput> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column();
+  }
+}
