50d0fcb206ccf9835ef58b087b21f267ad2cbe1a
{jinsung} <jayhan0215@gmail.com>
2024-07-24 09:54:59 +0900

    Scoll for signup screen when the keyboard up

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -36,66 +36,69 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
-            fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background.png"),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 40,
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/appicon_white.png"),
-                const SizedBox(
-                  height: 80,
-                ),
-                LoginInput(
-                  controller: idController,
-                  title: "Email",
-                ),
-                SizedBox(
-                  height: 12.h,
-                ),
-                LoginInput(
-                  controller: pwController,
-                  title: "Password",
-                  type: "pw",
-                ),
-                SizedBox(
-                  height: 80.h,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    loginValidation(
-                      context,
-                      idController.text,
-                      pwController.text,
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(1.sw, 55.h),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(100),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40,
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/appicon_white.png"),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  LoginInput(
+                    controller: idController,
+                    title: "Email",
+                  ),
+                  SizedBox(
+                    height: 12.h,
+                  ),
+                  LoginInput(
+                    controller: pwController,
+                    title: "Password",
+                    type: "pw",
+                  ),
+                  SizedBox(
+                    height: 80.h,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      loginValidation(
+                        context,
+                        idController.text,
+                        pwController.text,
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(1.sw, 55.h),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                    child: Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.bold,
+                        color: ColorTheme.mainBlue,
+                      ),
                     ),
                   ),
-                  child: Text(
-                    "LOGIN",
-                    style: TextStyle(
-                      fontSize: 18.sp,
-                      fontWeight: FontWeight.bold,
-                      color: ColorTheme.mainBlue,
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -17,72 +17,86 @@ class _SignupState extends State<Signup> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 40,
-          ),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset("assets/images/appicon_white.png"),
-                const SizedBox(
-                  height: 80,
-                ),
-                LoginInput(
-                  controller: emailController,
-                  title: "Email",
-                ),
-                SizedBox(
-                  height: 12.h,
-                ),
-                LoginInput(
-                  controller: pwController,
-                  title: "Password",
-                  type: "pw",
-                ),
-                SizedBox(
-                  height: 12.h,
-                ),
-                LoginInput(
-                  controller: nameController,
-                  title: "Username",
-                  type: "username",
-                ),
-                SizedBox(
-                  height: 80.h,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    print("Signup");
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(1.sw, 55.h),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(100),
-                    ),
-                  ),
-                  child: Text(
-                    "SIGNUP",
-                    style: TextStyle(
-                      fontSize: 18.sp,
-                      fontWeight: FontWeight.bold,
-                      color: ColorTheme.mainBlue,
-                    ),
-                  ),
-                ),
-              ],
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        // resizeToAvoidBottomInset: false,
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/background.png"),
+              fit: BoxFit.cover,
             ),
           ),
+          child: LayoutBuilder(builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
+                ),
+                child: IntrinsicHeight(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset("assets/images/appicon_white.png"),
+                          const SizedBox(
+                            height: 80,
+                          ),
+                          LoginInput(
+                            controller: emailController,
+                            title: "Email",
+                          ),
+                          SizedBox(
+                            height: 12.h,
+                          ),
+                          LoginInput(
+                            controller: pwController,
+                            title: "Password",
+                            type: "pw",
+                          ),
+                          SizedBox(
+                            height: 12.h,
+                          ),
+                          LoginInput(
+                            controller: nameController,
+                            title: "Username",
+                            type: "username",
+                          ),
+                          SizedBox(
+                            height: 80.h,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              print("Signup");
+                            },
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(1.sw, 55.h),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                            ),
+                            child: Text(
+                              "SIGNUP",
+                              style: TextStyle(
+                                fontSize: 18.sp,
+                                fontWeight: FontWeight.bold,
+                                color: ColorTheme.mainBlue,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }),
         ),
       ),
     );
