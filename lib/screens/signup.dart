32b6f31a4ce989407128b8078df684803000f8d6
{jinsung} <jayhan0215@gmail.com>
2024-07-27 23:05:55 +0900

    Reforming Signup screen

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:workout_app/colors.dart';
 
 class Signup extends StatefulWidget {
@@ -15,8 +16,84 @@ class _SignupState extends State<Signup> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: ColorsTheme.gray200,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          color: ColorsTheme.gray200,
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 25,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(),
+                SizedBox(
+                  height: 120.h,
+                ),
+                Text(
+                  "Create\nAccount",
+                  style: TextStyle(
+                    fontSize: 40.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Fullname",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: ColorsTheme.gray600,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Container(
+                      width: 1.sw,
+                      height: 55.h,
+                      decoration: BoxDecoration(
+                        color: ColorsTheme.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 18,
+                        ),
+                        child: TextField(
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          cursorColor: ColorsTheme.gray500,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(
+                              Icons.person_outline_rounded,
+                              color: ColorsTheme.gray500,
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
