5d6a87c64f1983741abb701caefa78e79a6e1c91
{jinsung} <jayhan0215@gmail.com>
2024-09-08 15:51:24 +0900

    Refactoring emailValid's controller

--- lib/models/signup_model.dart
+++ lib/models/signup_model.dart
@@ -3,14 +3,12 @@ import 'package:flutter/material.dart';
 class SignupModel with ChangeNotifier {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
-  final TextEditingController emailValidController = TextEditingController();
   final TextEditingController pwController = TextEditingController();
 
   @override
   void dispose() {
     nameController.dispose();
     emailController.dispose();
-    emailValidController.dispose();
     pwController.dispose();
     super.dispose();
   }
